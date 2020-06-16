cdef27ce94af53c900584de9d423f23f115d6d73
Tom Bosmans <tom.94.bosmans@gmail.com>
2020-06-16 10:07:09 +0200

    cleanup readme and names used in spec

--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe 'SALES TAXES' do
     it 'returns correct receipt' do
       receipt = Receipt.new(
         items: [
-          Item.new(name: 'The Last Wish', type: :book, price: 12.49), # the book
-          Item.new(name: 'In Flames: I, The Mask (2019)', type: :cd, price: 14.99), # the music cd
-          Item.new(name: 'snickers', type: :food, price: 0.85) # the chocolate bar
+          Item.new(name: 'The Last Wish', type: :book, price: 12.49),
+          Item.new(name: 'In Flames: I, The Mask (2019)', type: :cd, price: 14.99),
+          Item.new(name: 'Nougatti', type: :food, price: 0.85)
         ]
       )
 
@@ -18,7 +18,7 @@ RSpec.describe 'SALES TAXES' do
         eq(
           'The Last Wish': 12.49,
           'In Flames: I, The Mask (2019)': 16.49,
-          snickers: 0.85,
+          'Nougatti': 0.85,
           total_tax: 1.50,
           total_cost: 29.83
         )
@@ -30,15 +30,15 @@ RSpec.describe 'SALES TAXES' do
     it 'returns correct receipt' do
       receipt = Receipt.new(
         items: [
-          Item.new(name: 'box of snickers', type: :food, price: 10.00, imported: true),
-          Item.new(name: "box of L'Oreal", type: :parfum, price: 47.50, imported: true)
+          Item.new(name: 'Box of Bounty', type: :food, price: 10.00, imported: true),
+          Item.new(name: "Box of L'Oreal", type: :parfum, price: 47.50, imported: true)
         ]
       )
 
       expect(receipt.to_h).to(
         eq(
-          'box of snickers': 10.50,
-          "box of L'Oreal": 54.65,
+          'Box of Bounty': 10.50,
+          "Box of L'Oreal": 54.65,
           total_tax: 7.65,
           total_cost: 65.15
         )
@@ -51,18 +51,18 @@ RSpec.describe 'SALES TAXES' do
       receipt = Receipt.new(
         items: [
           Item.new(name: "L'Oreal", type: :parfum, price: 27.99, imported: true),
-          Item.new(name: "Fake L'Oreal", type: :parfum, price: 18.99, imported: false),
-          Item.new(name: 'pills for headache', type: :medical, price: 9.75, imported: false),
-          Item.new(name: 'box of snickers', type: :food, price: 11.25, imported: true)
+          Item.new(name: "Ma liberte", type: :parfum, price: 18.99, imported: false),
+          Item.new(name: 'Excedryn', type: :medical, price: 9.75, imported: false),
+          Item.new(name: 'Box of Bounty', type: :food, price: 11.25, imported: true)
         ]
       )
 
       expect(receipt.to_h).to(
         eq(
           "L'Oreal": 32.19,
-          "Fake L'Oreal": 20.89,
-          'pills for headache': 9.75,
-          'box of snickers': 11.85,
+          "Ma liberte": 20.89,
+          'Excedryn': 9.75,
+          'Box of Bounty': 11.85,
           total_tax: 6.70,
           total_cost: 74.68
         )
